7412205ad1a4889b0380a81ad8761c344c9c5eef
Wahmane-Hamza <hamza.wahm5@gmail.com>
2025-01-26 17:12:32 +0100

    sad

--- commands_help.c
+++ commands_help.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:39:01 by hwahmane          #+#    #+#             */
-/*   Updated: 2025/01/25 10:42:27 by hwahmane         ###   ########.fr       */
+/*   Updated: 2025/01/25 14:10:24 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ void	failed_fork(int *fd)
 	perror("error");
 	close(fd[1]);
 	close(fd[0]);
-	exit(1);
+	exit(127);
 }
 
 void	failed_pipe(void)
 {
 	perror("error");
-	exit(1);
+	exit(127);
 }
 
 void	input_error(char *arg_ex)
 {
 	ft_putstr_fd("Bad arguments\n", 2);
 	ft_putstr_fd(arg_ex, 2);
-	exit(1);
+	exit(127);
 }
