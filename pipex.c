b81272b53e17c1c64f556029fd64e03a33103d5a
Wahmane-Hamza <hamza.wahm5@gmail.com>
2025-01-26 17:12:42 +0100

    ad

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 14:54:56 by hwahmane          #+#    #+#             */
-/*   Updated: 2025/01/25 14:13:28 by hwahmane         ###   ########.fr       */
+/*   Updated: 2025/01/26 17:02:38 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ void	exec(char *cmd, char **env)
 	check(cmd, NULL);
 	commands = ft_split(cmd, ' ');
 	if (!commands)
-		ft_write(NULL, NULL, "Memory allocation failed", 127);
+		ft_write(NULL, NULL, "Memory allocation failed", 1);
 	check_arg(cmd, commands, env);
 	path = take_path(commands, commands[0], env);
 	if (!path)
-		ft_write(commands, NULL, ": not found", 127);
+		ft_write(commands, NULL, ": not found", 1);
 	execve(path, commands, env);
-	ft_write(commands, path, ": not found", 127);
+	ft_write(commands, path, ": not found", 1);
 }
 
 void	redir(char *cmd, char **env)
@@ -43,14 +43,14 @@ void	redir(char *cmd, char **env)
 	{
 		close(pipefd[1]);
 		if (dup2(pipefd[0], STDIN) == -1)
-			exit(127);
+			exit(1);
 		close(pipefd[0]);
 	}
 	else
 	{
 		close(pipefd[0]);
 		if (dup2(pipefd[1], STDOUT) == -1)
-			exit(127);
+			exit(1);
 		close(pipefd[1]);
 		exec(cmd, env);
 	}
@@ -69,20 +69,20 @@ void	redir2(char *cmd, char **env, int fdout)
 	if (pid == 0)
 	{
 		close(pipefd[0]);
-		if (fdout == -1)
-			if (dup2(pipefd[1], STDOUT) == -1)
-				exit(127);
 		if (fdout != -1)
 			if (dup2(fdout, STDOUT) == -1)
-				exit(127);
-		close(pipefd[1]);
+				exit(1);
 		if (fdout != -1)
 			close(fdout);
+		if (fdout == -1)
+			if (dup2(pipefd[1], STDOUT) == -1)
+				exit(1);
+		close(pipefd[1]);
 		exec(cmd, env);
 	}
 	close(pipefd[1]);
 	if (dup2(pipefd[0], STDIN) == -1)
-		exit(127);
+		exit(1);
 	close(pipefd[0]);
 }
 
@@ -99,27 +99,26 @@ void	redir3(char *cmd, char **env, int fdin)
 	if (pid == 0)
 	{
 		close(pipefd[0]);
-		if (fdin != -1)
-			if (dup2(fdin, STDIN) == -1)
-				exit(127);
-		if (fdin != -1)
-			close(fdin);
 		if (dup2(pipefd[1], STDOUT) == -1)
-			exit(127);
+			exit(1);
 		close(pipefd[1]);
+		if (dup2(fdin, STDIN) == -1)
+			exit(1);
+		close(fdin);
 		exec(cmd, env);
 	}
 	close(pipefd[1]);
-	if (dup2(pipefd[0], STDIN_FILENO) == -1)
-		exit(127);
+	if (dup2(pipefd[0], STDIN) == -1)
+		exit(1);
 	close(pipefd[0]);
 }
 
 int	main(int ac, char **av, char **env)
 {
-	int	i;
-	int	fdout;
-	int	fdin;
+	int		i;
+	int		fdout;
+	int		fdin;
+	t_data	data;
 
 	i = 3;
 	if (ac >= 5)
@@ -136,9 +135,10 @@ int	main(int ac, char **av, char **env)
 		redir2(av[i], env, fdout);
 		if (fdout != -1)
 			close(fdout);
-		while (wait(NULL) != -1) 
-			continue;
-
+		data.exit_num = wait_child(data);
+		if (fdout == -1)
+			data.exit_num = 1;
+		exit(data.exit_num);
 	}
 	else
 		input_error("Ex: ./pipex <file1> <cmd1> <cmd2> ... <cmdn> <file2>\n");
